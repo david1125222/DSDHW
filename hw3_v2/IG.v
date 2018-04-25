1498ae53c542534716daac386bb78d79e0667710
david1125222 <b04901044@ntu.edu.tw>
2018-04-25 18:44:45 +0800

    a

---
+++ hw3_v2/IG.v
@@ -0,0 +1,25 @@
+module IG ( clk , reset, done, img_wr, img_rd, img_addr, img_di, img_do, 
+            grad_wr, grad_rd, grad_addr, grad_do, grad_di);
+
+input clk, reset;
+input [7:0] img_di;
+input [19:0] grad_di;
+output done, img_wr, img_rd, grad_wr, grad_rd;
+output [15:0] img_addr, grad_addr;
+output [7:0] img_do;
+output [19:0] grad_do;
+
+//------------------------------------------------------------------
+// reg & wire
+
+
+//------------------------------------------------------------------
+// combinational part
+
+
+//------------------------------------------------------------------
+// sequential part
+
+
+
+endmodule
