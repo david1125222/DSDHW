1d1253f097225e33a971e84c1331541bc13f4e54
david1125222 <b04901044@ntu.edu.tw>
2018-05-11 15:26:34 +0800

    1

--- DSD_HW3/verilog/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++ DSD_HW3/verilog/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -1,7 +1,9 @@
-1525689132 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/HSs18n_128x32.v
+1525938503 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/HSs18n_128x32.v
 1525882061 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3.v
-1525883880 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3.vg
-1525882717 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/tsmc13.v
+1526022992 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3.vg
+1525932079 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_David.v
+1525943276 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_2.vg
+1525937862 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/tsmc13.v
 1525799477 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_tb.v
-1525882870 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_tb_gate.v
+1526023228 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_tb_gate.v
 1525796092 /home/raid7_2/userb04/b04044/DSDHW/DSD_HW3/verilog/DSDHW3_1.v
